b3028fe995ab79185eac4db45a9d53e83cb0eb3c
federicamudu <federica.mudu.1993@gmail.com>
2022-03-25 15:35:08 +0100

    prova week 4

---
+++ JavaScriptFilm.js
@@ -0,0 +1,25 @@
+﻿function subscribe() {
+    let name = document.getElementById("nameKey").value;
+    
+    localStorage.setItem("nameKey", name);
+    
+}
+
+function unsubscribe() {
+    
+    localStorage.removeItem("nameKey");
+    let key = document.getElementById("nameKey").value;
+    localStorage.removeItem(key);
+    document.getElementById("result").innerHTML = "Eliminato Item con nome " + key;
+    
+}
+
+function welcomeMessage() {
+    if (localStorage.getItem("nameKey").value == null) {
+        alert("Benvenuto!");
+    }
+    else {
+        return null;
+    }
+    
+}
